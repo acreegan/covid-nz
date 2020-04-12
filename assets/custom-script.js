c70958a9b5dc1c6bc144041cada62f221dc68560
Andrew <andrew.s.creegan@gmail.com>
2020-04-12 16:34:39 +1200

    Added tabs. Added a graph with daily increase to total

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -10,13 +10,30 @@ function allDescendantsAutocompleteOff (node) {
 }
 
 
-function func(node)
+function func(id)
 {
-    allDescendantsAutocompleteOff(document.getElementById("countries"))
+    allDescendantsAutocompleteOff(document.getElementById(id))
 }
 
+if (!window.dash_clientside) {
+  window.dash_clientside = {};
+}
+window.dash_clientside.clientside = {
+  autocomplete_off: function(value) {
+    if (value == "tab-1"){
+        setTimeout(func,500,"countries")
+    }
+    else{
+        setTimeout(func,500,"countries2")
+    }
 
-setTimeout(func,5000)
+    return null;
+  }
+};
+
+
+// Run on load (need func so get element doesn't get evaluated until after timeout)
+setTimeout(func,5000,"countries")
 
 
 
